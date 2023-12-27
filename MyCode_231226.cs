41078cba4ba29276459fa2d481be4cdbf8a1659b
Oh-Nahyun <101961487+Oh-Nahyun@users.noreply.github.com>
2023-12-27 11:34:58 +0900

    Update and rename Program.cs to MyCode_231226.cs

--- MyCode_231226.cs
+++ MyCode_231226.cs
@@ -1,26 +1,28 @@
 ﻿namespace _01_Console
 {
-    internal class Program
+    internal class MyCode_231226
     {
         static void Main(string[] args)
         {
             /*
-            // 출력
+            ///231226
+            
+            /// 출력
             Console.WriteLine("Hello, World!");
             Console.WriteLine("오나현");           // 한 줄 출력 코드
             Console.Write("dkdkdk");              // 글자 출력 코드
             Console.Write("대대대\n아아아\n");     // 엔터
 
-            // 입력
-            // 변수 (Variable) : 변하는 숫자, 메모리에 기록해둔 값
-            // 키보드 입력을 한 줄 받아서 input이라는 변수에 기록하기
+            /// 입력
+            /// 변수 (Variable) : 변하는 숫자, 메모리에 기록해둔 값
+            /// 키보드 입력을 한 줄 받아서 input이라는 변수에 기록하기
             string input = Console.ReadLine();
             Console.WriteLine(input);             // input 변수의 내용 출력
             */
 
 
-            // 실습
-            // 시작하면 이름을 물어보고 이름을 3번 출력하는 코드 만들기
+            /// 실습
+            /// 시작하면 이름을 물어보고 이름을 3번 출력하는 코드 만들기
             string MyName = Console.ReadLine();
             Console.WriteLine(MyName);
             Console.WriteLine(MyName);
